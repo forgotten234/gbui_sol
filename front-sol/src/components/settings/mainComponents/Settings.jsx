ead2b4a1839e91b49ebeb467b25d50d8e719ca38
Tim S <tim.seegert@student.htw-berlin.de>
2021-08-31 16:11:41 +0200

    add style for settings

--- front-sol/src/components/settings/mainComponents/Settings.jsx
+++ front-sol/src/components/settings/mainComponents/Settings.jsx
@@ -1,15 +1,13 @@
 import React, {useState, useEffect, useContext} from 'react'
 import { Link } from "react-router-dom"
 import { Button } from 'react-bootstrap'
-//import SettingsUser from './SettingsUser'
+import SettingsUser from './SettingsUser'
 //import SettingsAdmin from './SettingsAdmin'
 
 const Settings = () => {
     return (
-        <>
-            <Button variant="outline-info">
-                <Link to="/">Welcome page</Link>
-            </Button>
+        <>  
+            <SettingsUser />
         </>
     )
 }
--- front-sol/src/components/settings/mainComponents/SettingsUser.jsx
+++ front-sol/src/components/settings/mainComponents/SettingsUser.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect, useContext} from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import {AuthContext} from '../../contexts/AuthContext'
 //see below role comment
 //import {RoleContext} from '../contexts/RoleContext'
+import './styles.css'
 
 export default function SettingsUser(){
     const { auth } = useContext(AuthContext)
@@ -11,18 +12,21 @@ export default function SettingsUser(){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [userData, setUserData] = useState()
-    
+    const [showFormForChangingPersonalData, setShowFormForChangingPersonalData] = useState(false)
+    const [showBuisList, setShowBuisList] = useState(false)
+
     //get all information about the user to show them as placeholder in the fields below when auth is ready to use
     useEffect(() => {
         if(auth.data !== null){
             fetchData()
+            console.log(userData)
         }     
     }, [auth])
 
 
     //this and the useeffect needs to be in the main settings page 
     const fetchData = async () => {
-        await fetch('http://localhost:9001/users/get-user/' + auth.data.id)
+        await fetch('http://localhost:9001/users/get-user/' + auth.data.userId)
             .then(response => response.json())
             .then(data => {
                 setPassword(data.password)
@@ -34,7 +38,7 @@ export default function SettingsUser(){
     //by submitting the form all typed in data will be updated
     const onFormSubmit = async (e) => {
         e.preventDefault()
-        await fetch('http://localhost:9001/users/update-user/' + auth.data.id, {
+        await fetch('http://localhost:9001/users/update-user/' + auth.data.userId, {
             method: 'PATCH',
             body: JSON.stringify({
                 email: email,
@@ -53,64 +57,70 @@ export default function SettingsUser(){
 
     //if no userData are there a loading component will be rendert till the userData are fetched
     return userData ? (
-        <div className="settingsContainer">
-          <Form onSubmit={onFormSubmit}>
-            <Form.Group>
-              <Form.Label>
-                Change Password
-              </Form.Label>
-              <Form.Control type="password" placeholder={"•••••••••••••"} onChange={(e)=> setPassword(e.target.value)}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change E-Mail
-              </Form.Label>
-              <Form.Control type="email" placeholder={email} onChange={(e)=> setEmail(e.target.value)}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Street
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.adress.street} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, street: e.target.value}})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Code Postal
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.adress.cp} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, cp: e.target.value}})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Town
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.adress.town} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, town: e.target.value}})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Country
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.adress.country} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, country: e.target.value}})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Firm
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.firm} onChange={(e)=> setUserData({...userData, firm: e.target.value})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Name
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.name} onChange={(e)=> setUserData({...userData, name: e.target.value})}/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>
-                Change Surname
-              </Form.Label>
-              <Form.Control type="textarea" placeholder={userData.surname} onChange={(e)=> setUserData({...userData, surname: e.target.value})}/>
-            </Form.Group>
-            <Button variant="outline-info" type="submit">Submit</Button>
-          </Form>
+        <div style={{marginTop: "50px"}}>
+            <Container className="containerHeader">
+                <p className="squareLeft">&#9725;</p>
+                <p className="squareRight">&#9725;</p>
+            </Container>
+            <Container className="containerBody" fluid="md">
+                <Row>
+                    <Col className="d-flex justify-content-center">
+                        <div className="settingsButtonArea">
+                            <div className="settingsButton d-grid gap-2">
+                                <Button variant="outline-light" onClick={() => setShowBuisList(prevState => !prevState)}>
+                                    Show Buis
+                                </Button>
+                            </div>
+                            <div className="settingsButton d-grid gap-2">
+                                <Button variant="outline-light" onClick={() => setShowFormForChangingPersonalData(prevState => !prevState)}>
+                                    Change personal data
+                                </Button>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col className="d-flex justify-content-center">
+                        {
+                            showFormForChangingPersonalData
+                            ?
+                                <Form onSubmit={onFormSubmit} className="settingsForm">
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="password" placeholder={"•••••••••••••"} onChange={(e)=> setPassword(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="email" placeholder={email} onChange={(e)=> setEmail(e.target.value)}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.adress.street} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, street: e.target.value}})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.adress.cp} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, cp: e.target.value}})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.adress.town} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, town: e.target.value}})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.adress.country} onChange={(e)=> setUserData({...userData, adress: {...userData.adress, country: e.target.value}})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">                       
+                                        <Form.Control type="textarea" placeholder={userData.firm} onChange={(e)=> setUserData({...userData, firm: e.target.value})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.name} onChange={(e)=> setUserData({...userData, name: e.target.value})}/>
+                                    </Form.Group>
+                                    <Form.Group className="inputSettingsForm">
+                                        <Form.Control type="textarea" placeholder={userData.surname} onChange={(e)=> setUserData({...userData, surname: e.target.value})}/>
+                                    </Form.Group>
+                                    <div className="submitArea">
+                                        <Button variant="outline-light" type="submit" className="outline-light">
+                                            <div className="submitParagraph">Submit Changes</div>
+                                        </Button>
+                                    </div>
+                                </Form>
+                            :   <></>
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </div>
       ) : (
           <div>Loading..</div>
