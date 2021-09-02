a212eebb8e8b4c6871944f6d50c37c7334f550fe
s0576994 <leroy.hawelky@student.htw-berlin.de>
2021-09-02 13:01:47 +0200

    added container and restrucered bui Cards

--- front-sol/src/components/BuiCards/BuiCardShow.jsx
+++ front-sol/src/components/BuiCards/BuiCardShow.jsx
@@ -6,9 +6,17 @@ import BuiCard from './BuiCard'
 function BuiCardShow(){
 
     return(
+<<<<<<< HEAD:front-sol/src/components/main/BuiCardShow.jsx
         <div >
             <h4 className="text-uppercase">Hoch Bewertete Buis</h4>
             <BuiCard/>
+=======
+        <div>
+            <Container>
+              <h4 className="text-uppercase">Hoch Bewertete Buis</h4>
+              <BuiCard/>
+            </Container>
+>>>>>>> added container and restrucered bui Cards:front-sol/src/components/BuiCards/BuiCardShow.jsx
         </div>
     )
 }
